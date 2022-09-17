4a66093ec568c65b13e7b655a6834c0c7089251f
Johannes Thorén <johannes@lgjt.xyz>
2022-09-17 16:22:05 +0200

    primitive file upload

---
+++ data/files/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct {int x; int y;} vec_2;
+
+void x(int x) {printf("%c %d",x ,x);}
+
+int main() {
+	vec_2 vec = {x: 10, y: 10};
+	int x = 100;
+	return 0;
+}
+
--- server.py
+++ server.py
@@ -2,9 +2,10 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 import os
 from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
 
 load_dotenv()
 
@@ -17,10 +18,17 @@ def index():
 
 @app.post("/upload")
 def upload():
-    # TODO: handle file upload and mail confirmation
-    return "poof"
+    # TODO: well we can save file, but it should not be saved as a something dot something file.
+    # it should have a UID as file name. then the file path should sent to the db together with
+    # the password and email address.
+
+    file = request.files['file']
+    open(os.environ.get("FILE_STORAGE_LOCATION")+file.filename,"w+").close()
+    file.save(os.environ.get("FILE_STORAGE_LOCATION")+"/"+file.filename)
+
+    return render_template("file-details.html", password=request.form.get("Password"), filename=file.filename, )
 
 
 
 if __name__ == "__main__":
-    app.run(port=os.environ.get("PORT"), host=os.environ["HOSTNAME"])
+    app.run(port=os.environ.get("PORT"), host=os.environ["HOSTNAME"], debug=True)
