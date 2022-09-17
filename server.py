102e9dce5ad12aa609e24f528f88ee0c1bba7f4d
Johannes Thorén <johannes@lgjt.xyz>
2022-09-17 13:05:46 +0200

    added license: BSD-3 license

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+# Copyright 2022 Johannes Thorén. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
