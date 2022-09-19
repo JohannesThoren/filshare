b53e18fc0a85c2d1161065242fb87e05caa22538
Johannes Thorén <johannes@lgjt.xyz>
2022-09-19 14:36:44 +0200

    implemented password protection

--- server.py
+++ server.py
@@ -44,18 +44,32 @@ def upload():
     url = f"http://{os.environ.get('HOSTNAME')}:{os.environ.get('PORT')}/f/{uid}"
     date = datetime.datetime.now()
 
-
-    db.add_file(uid, file.filename, email, md5(file_password.encode("UTF-8")).hexdigest(), date=date)
+    if(file_password != ""):
+        db.add_file(uid, file.filename, email, md5(file_password.encode("UTF-8")).hexdigest(), date=date)
+    else:
+        db.add_file(uid, file.filename, email, "", date=date)
 
     return render_template("file-details.html", password=file_password, filename=file.filename, url=url)
 
 
-@app.get('/f/<uid>')
+@app.route('/f/<uid>', methods=["GET", "POST"])
 def download(uid):
     fd = db.get_file_information(uid)
-    file_path = os.environ.get("FILE_STORAGE_LOCATION")
+    passwd = request.form.get("Password")
 
-    return send_file(f"{file_path}/{uid}", download_name=fd[1], )
+    if request.method == "POST":
+        if md5(passwd.encode("UTF-8")).hexdigest() == fd[2]:
+            file_path = os.environ.get("FILE_STORAGE_LOCATION")
+            return send_file(f"{file_path}/{uid}", download_name=fd[1], )
+
+
+    if request.method == "GET":          
+        if fd[2] and not passwd:
+            url = f"http://{os.environ.get('HOSTNAME')}:{os.environ.get('PORT')}/f/{fd[0]}"
+            return render_template("enter-password.html", url=url)
+        else:
+            file_path = os.environ.get("FILE_STORAGE_LOCATION")
+            return send_file(f"{file_path}/{uid}", download_name=fd[1],)
 
 
 if __name__ == "__main__":
